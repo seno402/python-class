3fb5db5144e341885fb7d2939694a9f2b7c895e7
seno402 <realllbob2020@gmail.com>
2025-05-03 11:59:16 +0100

    lesson 2

---
+++ lesson 2/activity1.py
@@ -0,0 +1,10 @@
+#print a sentence
+print ("hi i am seno")
+#print a number 
+print (12)
+#next line after print
+print ("hello\n","seno")
+#pass multiple values in print 
+print ("seno",12)
+#ending statement with any symbol in print 
+print ("hope you find this activity easy",end="=)")
