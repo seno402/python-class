cac6bef413ee5a79bda6d7e82e2598395e65f7fa
seno402 <realllbob2020@gmail.com>
2025-05-03 12:15:03 +0100

    lesson 2 activity 2

---
+++ lesson 2/activity2.py
@@ -0,0 +1,9 @@
+#understanding vaiables and creating different variables
+name="seno"
+numb=18
+print (name)
+print (numb)
+print (name,numb)
+#input from the user
+a= input("what country do you live in?\n")
+print ("hello",name ,"nice to meet you so you are from ",a)
