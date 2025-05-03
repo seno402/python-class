87aa04d17aaa2f06a04aaf48f6668d30d6b7f5c5
seno402 <realllbob2020@gmail.com>
2025-05-03 12:24:42 +0100

    lesson 2 activity 3

---
+++ lesson 2/activity 3.py
@@ -0,0 +1,4 @@
+#printing all the keywords in python 
+import keyword
+print (" keywords are reserved and special words that tell a simple and more defined defination in python")
+print (keyword.kwlist)
