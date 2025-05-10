ac9458d5ab53ad05cffcf55477abf877c16d37e6
seno402 <realllbob2020@gmail.com>
2025-05-10 10:45:21 +0100

    activity 4 hw

---
+++ lesson 2/activity4.py
@@ -0,0 +1,5 @@
+Dim birthday:Year As Integer = 2000
+Dim birthday:Month As Integer = 12
+Dim birthday:Day As Integer = 25
+Dim birthday:Date As DateTime = New DateTime(2000, 12, 25)
+Dim birthday:AsString As String = "12/25/2000"
