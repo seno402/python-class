543293f81717241de472fc753b863996f08faeed
seno402 <realllbob2020@gmail.com>
2025-05-10 12:34:28 +0100

    lesson 3

---
+++ lesson 3 data types in python/activity2.py
@@ -0,0 +1,19 @@
+# type casting means to change the data type of a variable 
+a=7 
+print (a , type(a))
+b=4.2
+print (b , type(b))
+c=True
+print (c , type(c))
+d="hi seno" 
+print (d , type(d))
+# type casting to convert the data type
+a1= float(a)
+print (a1 , type (a1))
+p=int (b)
+print (p , type(p))
+s=str (c)
+print (s, type(s))
+# taking imput from user and printing it
+l= input ("enter a number of your choice")
+print (l)
---
+++ lesson 3 data types in python/activity3.py
@@ -0,0 +1,17 @@
+# joining two strings 
+string1="meet my dog"
+string2="lila"
+print(string1+string2)
+# changing the case 
+m= "i love india"
+print (m.upper())
+m2= " I LOVE INDIA"
+print (m2.lower())
+# calculate the lenth of a string
+z="find my lenth"
+print (len(z))
+# reversing a string
+text=input("enter your text to reverse")
+f=text[::-1]
+text=f
+print(text)
