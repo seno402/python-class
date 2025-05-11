9b67ca0100351cb6cd40f9c915b184484300fa8f
seno402 <realllbob2020@gmail.com>
2025-05-11 18:37:25 +0100

    HW

---
+++ lesson 3 data types in python/activity4.py
@@ -0,0 +1,15 @@
+n1="seno"
+n2="rodi"
+n3="somatra"
+n4="medha"
+n5="serah"
+b1="2/5/2012"
+b2="16/6/2013"
+b3="30/12/2011"
+b4="1/9/2012"
+b5="6/7/2013"
+print (n1 , b1)
+print (n2 , b2)
+print (n3 , b3)
+print (n4 , b4)
+print (n5 , b5)
