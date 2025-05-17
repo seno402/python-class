76be416fb757fca6e32ad9ebdfa84b1d3ff7f2a9
seno402 <realllbob2020@gmail.com>
2025-05-17 12:00:07 +0100

    lesson 4 activity 1

---
+++ lesson 4/activity1.py
@@ -0,0 +1,12 @@
+#storing values
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+
+sum=tree1+tree2+tree3+tree4+tree5 
+print("sum of all tree values:",sum)
+
+average=sum/5
+print ("average of all trees is :", average)
