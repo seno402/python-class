5b1ec008dfdfeea359a7064ddf7a08c193c0fee3
seno402 <realllbob2020@gmail.com>
2025-06-02 12:55:37 +0100

    activity1

---
+++ lesson 6/activity1.py
@@ -0,0 +1,20 @@
+a=7
+b=4
+c=2
+if a and b and c:
+    print ("all the numbers have boolean values as true")
+else:
+    print("atleat one number has boolean value as false")
+    
+a=7
+b=-8
+c=0
+if a > 0 or b > 0:
+    print ("either of the number is greater than 0")
+else:
+    print("number is not greater than 0")
+    
+if b > 0 or c > 0:
+    print("either of the number is greater than 0")
+else:
+    print("the number is not greater than 0")
