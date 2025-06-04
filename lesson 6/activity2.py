85a69a586495a3d8216fc5b255b10139d9dc9e67
seno402 <realllbob2020@gmail.com>
2025-06-04 18:33:06 +0100

    hw.py

---
+++ lesson 6/activity2.py
@@ -0,0 +1,25 @@
+a=7
+b=20
+c=20
+
+print (a !=b)
+print (b !=c)
+
+
+a= "food"
+b= "phone"
+
+if a !=b:
+    print (a , "and" , b , "are different")
+    
+    
+a=4
+b=17
+
+if (a ==4 )!=(b==17):
+    print ("hello")
+
+
+a=int(input("enter a number"))
+if a % 2 != 0:
+    print (a , "is not a even number")
