601247de42bbda40b6456e19d8625b0267e2f639
seno402 <realllbob2020@gmail.com>
2025-06-07 11:57:03 +0100

    activity1.py

---
+++ lesson 8/activity1.py
@@ -0,0 +1,13 @@
+a=7
+b=8
+c=9
+d=2
+e=(a+b)*c/d
+print("value of (a+b)*c/d is" , e) 
+
+name="seno"
+age=12
+if name== "seno" or name=="bob" and age>=2:
+    print("hello welcome")
+else: 
+    print("goodbye")
