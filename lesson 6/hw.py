9768de0fd65b8ffb14020838369681d35e62c261
seno402 <reallbob2020@gmail.com>
2026-02-10 18:52:03 +0000

    Delete lesson 6 directory

--- lesson 6/hw.py
+++
@@ -1,6 +0,0 @@
-# somevalue = input("enter anything with a value :")
-# # print (somevalue)
-# if somevalue.isdigit():
-#     print("The value entered is a number")
-# else:
-#     print("the value is a letter")
