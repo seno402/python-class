370cd2ee9bffc5b45a6127f129a6967bd84ab904
seno402 <reallbob2020@gmail.com>
2026-02-10 18:49:07 +0000

    Delete class1 directory

--- class1/ac2.py
+++
@@ -1,4 +0,0 @@
-first_name="seno"
-last_name="thumma"
-full_name=first_name + last_name
-print(full_name)
--- class1/ac3.py
+++
@@ -1,4 +0,0 @@
-half_name1="himalya pvt"
-half_name2="limited"
-full_name=half_name1+half_name2
-print(full_name)
