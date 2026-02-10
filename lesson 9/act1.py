dd204930b7a73d1b43402b0dcb4c347a6377491f
seno402 <reallbob2020@gmail.com>
2026-02-10 18:51:34 +0000

    Delete lesson 9 directory

--- lesson 9/act1.py
+++
@@ -1,9 +0,0 @@
-medical_cause=input("did you have any medical reason yes or no?")
-attendance= int (input(" enter you attendance"))
-if medical_cause=="yes":
-    print("you are allowed")
-else:
-    if attendance>=75:
-        print("you are allowed")
-    else: 
-        print("you are not allowed")
