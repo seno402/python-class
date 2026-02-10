fa112f1675339d5b991553d6a8f2cb534f973dbe
seno402 <reallbob2020@gmail.com>
2026-02-10 18:52:54 +0000

    Delete lesson 2 directory

--- lesson 2/activity 3.py
+++
@@ -1,4 +0,0 @@
-#printing all the keywords in python 
-import keyword
-print (" keywords are reserved and special words that tell a simple and more defined defination in python")
-print (keyword.kwlist)
